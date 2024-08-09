efbae6aab57c8447bd7e44d99f662de9ad0dd47d
ricardopereira <ricardompb@outlook.com>
2024-08-09 16:46:12 -0300

    sequence com id

--- models/sequence.js
+++ models/sequence.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
   class sequence extends Model {}
 
   sequence.init({
-    id: { type: DataTypes.tenantId, primaryKey: true, allowNull: false },
+    id: { type: DataTypes.UUID, primaryKey: true, allowNull: false },
     type: { type: DataTypes.STRING, allowNull: false },
     value: { type: DataTypes.BIGINT, allowNull: false, defaultValue: 0 },
     tenantId: { type: DataTypes.UUID, allowNull: false },
